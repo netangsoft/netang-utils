3d2664d4d9c7c21997a2eef3d13e2cc12f6ae6a6
junmiao <mj287@163.com>
2022-03-16 12:05:30 +0800

    loader

--- webpackBuild.js
+++ webpackBuild.js
@@ -1,52 +1,6 @@
 const { promisify } = require('util')
 const webpack = require('webpack')
 const webpackPromisify = promisify(webpack)
-let webpackDevServer = null
-
-async function webpackServerWatch(webpackConfig) {
-    if (webpackDevServer === null) {
-        webpackDevServer = require('webpack-dev-server-ssr')
-    }
-    return await new Promise(function(resolve) {
-        const compiler = webpack(webpackConfig)
-        new webpackDevServer(compiler, {
-            // sockPort: configData.webPort,
-            // port: configData.webPort,
-            host: '0.0.0.0',
-            stats: {
-                // 添加资源信息
-                assets: true,
-                // 显示缓存的资源（将其设置为 `false` 则仅显示输出的文件）
-                cachedAssets: false,
-                // 添加 children 信息
-                children: false,
-                // 添加 chunk 信息（设置为 `false` 能允许较少的冗长输出）
-                chunks: false,
-                // 以不同颜色区分构建信息
-                colors: true,
-                // 添加构建模块信息
-                modules: false,
-                warnings: false,
-                entrypoints: false
-            },
-            // 关闭 webpack-dev-server 自带的 server info 信息
-            disableInfo: true,
-            disableHostCheck: true,
-            publicPath: '/',
-            hotOnly: true,
-            hot: true,
-            https: false,
-            clientLogLevel: 'error',
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
-                'Access-Control-Allow-Headers': 'X-Requested-With,content-type,Authorization'
-            }
-        })
-        compiler.hooks.done.tap('DonePlugin', resolve)
-        // server.listen(configData.webPort)
-    })
-}
 
 /**
  * 【node】webpack 打包
@@ -56,12 +10,8 @@ const webpackBuild = async (options) => {
 
     const promises = []
 
-    for (const { watch, configs } of options) {
-        if (watch === true) {
-            promises.push(webpackServerWatch(configs))
-        } else {
-            promises.push(webpackPromisify(configs))
-        }
+    for (const configs of options) {
+        promises.push(webpackPromisify(configs))
     }
 
     await Promise.all(promises)
