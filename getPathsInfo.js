18f9c7e3987b37acb277c880babc98671132e436
junmiao <mj287@163.com>
2022-03-16 09:03:41 +0800

    netang-utils

--- getPathsInfo.js
+++ getPathsInfo.js
@@ -4,21 +4,23 @@ const _ = require('lodash')
 const ROOT_PATH = require('./_rootPath')
 
 /**
- * 【node】获取所有根目录的路径
+ * 【node】获取所有根目录的路径详细
  */
-function getPaths() {
-    const paths = {
-        ROOT_PATH,
-    }
+function getPathsInfo() {
+    const pathsInfo = {}
     const files = fs.readdirSync(ROOT_PATH)
     for (let file of files) {
         if (! _.startsWith(file, '.')) {
             const fileKey = _.toUpper(_.snakeCase(file)) + '_PATH'
             const filePath = path.join(ROOT_PATH, file)
-            paths[fileKey] = filePath
+            pathsInfo[fileKey] = {
+                name: file,
+                path: filePath,
+            }
         }
     }
-    return paths
+
+    return pathsInfo
 }
 
-module.exports = getPaths
+module.exports = getPathsInfo
