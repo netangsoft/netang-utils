ea96755f261e3fe4e853e16f997a33e5183c5f70
junmiao <mj287@163.com>
2022-03-20 18:20:48 +0800

    random

--- getRandomString.js
+++ getRandomString.js
@@ -6,10 +6,11 @@ const _sample = require('lodash/sample')
  * @returns {string}
  */
 function getRandomString(length = 16) {
+    const pool = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     let str = ''
     for (let i = 0; i < length; i++) {
-        str += _sample(['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+        str += _sample(pool)
     }
     return str
 }
