7851ca94731f59ea592dc87730ff0fdf36c904dc
junmiao <mj287@163.com>
2022-04-29 10:34:42 +0800

    split

---
+++ split.js
@@ -0,0 +1,17 @@
+const isFillString = require('./isFillString')
+
+/*
+ * 拆分字符串
+ * @param {string} str 要拆分的字符串
+ * @param {string|RegExp} separator 拆分的分隔符
+ * @param {number} limit 限制结果的数量
+ * @returns {array}
+ */
+function split(str = '', separator, limit) {
+    if (isFillString(str)) {
+        return str.split(separator, limit)
+    }
+    return []
+}
+
+module.exports = split
