2ec7b56376b02a07c73c90bea15898f49f6445d6
junmiao <mj287@163.com>
2022-05-16 16:16:34 +0800

    filter

--- filter.js
+++ filter.js
@@ -1,27 +1,13 @@
-const _indexOf = require('lodash/indexOf')
-const isFillObject = require('./isFillObject')
-const isFillArray = require('./isFillArray')
+const _filter = require('./_filter')
 
 /**
- * 筛选数据
+ * 保留筛选数据
  * @param {object} data 筛选数据
- * @param {array} fields 筛选字段
- * @param {boolean} isRemove 是否移除字段
+ * @param {array} fields 保留筛选字段
  * @returns {object}
  */
-function filter(data, fields = [], isRemove = false) {
-
-    const obj = {}
-
-    if (isFillObject(data) && isFillArray(fields)) {
-        for (const key in data) {
-            if ((_indexOf(fields, key) === -1) === isRemove) {
-                obj[key] = data[key]
-            }
-        }
-    }
-
-    return obj
+function filter(data, fields = []) {
+    return _filter(data, fields, false)
 }
 
 module.exports = filter
