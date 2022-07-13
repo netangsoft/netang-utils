0d01cb91fa5e50b7cdf9c26cf4918d9a42fc8b21
junmiao <mj287@163.com>
2022-07-13 13:36:55 +0800

    replaceStar

--- replaceStar.js
+++ replaceStar.js
@@ -1,4 +1,6 @@
 const _isString = require('lodash/isString')
+const _isNumber = require('lodash/isNumber')
+const _trim = require('lodash/trim')
 
 /**
  * 替换为星号
@@ -11,23 +13,35 @@ const _isString = require('lodash/isString')
 
 function replaceStar(val, start = 1, end = 1, starCount = 0) {
 
-    if (! _isString(val)) {
-        val = String(val)
+    const isString = _isString(val)
+    if (! isString && ! _isNumber(val)) {
+        return '****'
     }
 
+    // 格式化字符串
+    val = isString ? _trim(val) : String(val)
+
+    // 获取字符长度
     const { length } = val
 
     // 开头
     let content = val.substr(0, start)
 
-    // 中间星号
-    const num = starCount || length - start - end
+    // 剩余数量
+    const remainCount = length - start - end
+
+    // 星号数量
+    const num = starCount > 0 ? starCount : (remainCount > 0 ? remainCount : 4)
     for (let i = 0; i < num; i++) {
         content += '*'
     }
 
-    // 最后部分
-    return content + val.substr(length - start - 1)
+    // 如果有剩余数量
+    if (end > 0 && remainCount > 0) {
+        content += val.substr(length - end)
+    }
+
+    return content
 }
 
 module.exports = replaceStar
