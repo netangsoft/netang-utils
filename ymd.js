69fdc05c1fc7faf3f61a290d40e2021272d24c6a
junmiao <mj287@163.com>
2022-12-24 10:34:58 +0800

    ymd

--- ymd.js
+++ ymd.js
@@ -4,12 +4,16 @@ const isNumeric = require('./isNumeric')
 /**
  * 数字年月日
  */
-
-function toString(value, defaultValue = '') {
+function toString(value, separator = '-', defaultValue = '') {
     if (isNumeric(value)) {
         value = String(value)
-        if (value.length === 8) {
-            return `${value.substr(0, 4)}-${value.substr(4, 2)}-${value.substr(6, 2)}`
+        const isLen8 = value.length === 8
+        if (isLen8 || value.length === 6) {
+            let val = `${value.substr(0, 4)}${separator}${value.substr(4, 2)}${separator}${value.substr(6, 2)}`
+            if (isLen8) {
+                val += `${separator}${value.substr(6, 2)}`
+            }
+            return val
         }
     }
     return defaultValue
@@ -18,11 +22,12 @@ function toString(value, defaultValue = '') {
 function toNumber(value, separator = '-') {
     if (_isString(value)) {
         const arr = value.split(separator)
+        const isLen3 = arr.length === 3
         if (
-            arr.length === 3
+            (isLen3 || arr.length === 2)
             && arr[0].length === 4
             && arr[1].length === 2
-            && arr[2].length === 2
+            && (! isLen3 || arr[2].length === 2)
         ) {
             return Number(arr.join(''))
         }
