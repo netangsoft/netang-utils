1798f830924269d7668120f2272966e3537e1ee1
junmiao <mj287@163.com>
2022-12-26 08:38:18 +0800

    tree

--- tree.js
+++ tree.js
@@ -4,16 +4,22 @@ const run = require('./run')
 
 /**
  * 获取树数据
- * @param {object} p
+ * @param {object} params
  * @returns {object}
  */
 
-function tree(p) {
-    p = Object.assign({
+function tree(params) {
+
+    const o = Object.assign({
+        // 数据
         data: [],
-        isRoot: false,
-        hasChildren: true,
+        // 是否存在根节点
+        root: false,
+        // 根节点名称
         rootTitle: '根目录',
+        // 叶子是否含 children 字段
+        leafHasChildren: true,
+
         idKey: 'id',
         pidKey: 'pid',
         titleKey: 'title',
@@ -23,8 +29,9 @@ function tree(p) {
         attrKey: 'attr',
         childrenKey: 'children',
 
-        fnFormat: null,
-    }, p)
+        // 格式化单个元素
+        format: null,
+    }, params)
 
     const all = {}
     const allTree = {}
@@ -33,45 +40,45 @@ function tree(p) {
     // tree 默认展开id
     const expand = []
 
-    const len = isFillArray(p.data) ? p.data.length : 0
+    const len = isFillArray(o.data) ? o.data.length : 0
     if (len) {
 
         // 获取 all 数据
         for (let i = 0; i < len; i++) {
-            const item = p.data[i]
-            const index = item[p.idKey]
+            const item = o.data[i]
+            const index = item[o.idKey]
             all[index] = item
 
-            if (_isNil(item[p.pidKey])) {
-                item[p.pidKey] = 0
+            if (_isNil(item[o.pidKey])) {
+                item[o.pidKey] = 0
             }
 
             allTree[index] = {}
-            allTree[index][p.valueKey] = index
-            allTree[index][p.textKey] = item[p.titleKey]
-            allTree[index][p.attrKey] = item
+            allTree[index][o.valueKey] = index
+            allTree[index][o.textKey] = item[o.titleKey]
+            allTree[index][o.attrKey] = item
 
-            if (p.hasChildren) {
-                allTree[index][p.childrenKey] = []
+            if (o.leafHasChildren) {
+                allTree[index][o.childrenKey] = []
             }
 
             // 格式化单个数据
-            run(p.fnFormat)(allTree[index], item, p)
+            run(o.format)(allTree[index], item, p)
         }
 
         // 获取 tree 数据
         for (let i = 0; i < len; i++) {
-            const item = allTree[p.data[i][p.idKey]]
+            const item = allTree[o.data[i][o.idKey]]
 
             // 以当前遍历项的 pid, 去 all 对象中找到索引的 id
-            const parent = item[p.attrKey][p.pidKey] > 0 ? allTree[item[p.attrKey][p.pidKey]] : false
+            const parent = item[o.attrKey][o.pidKey] > 0 ? allTree[item[o.attrKey][o.pidKey]] : false
 
             // 如果找到索引, 那么说明此项不在顶级当中,那么需要把此项添加到, 他对应的父级中
             if (parent) {
-                if (isFillArray(parent[p.childrenKey])) {
-                    parent[p.childrenKey].push(item)
+                if (isFillArray(parent[o.childrenKey])) {
+                    parent[o.childrenKey].push(item)
                 } else {
-                    parent[p.childrenKey] = [item]
+                    parent[o.childrenKey] = [item]
                 }
 
             // 否则没有在 all 中找到对应的索引 id, 那么直接把 当前的item添加到 tree 结果集中, 作为顶级
@@ -82,16 +89,16 @@ function tree(p) {
     }
 
     // 是否存在根目录
-    if (p.isRoot) {
+    if (o.root) {
         const first = {}
-        first[p.valueKey] = 0
-        first[p.textKey] = p.rootTitle
+        first[o.valueKey] = 0
+        first[o.textKey] = o.rootTitle
 
-        first[p.attrKey] = {}
-        first[p.attrKey][p.idKey] = 0
-        first[p.attrKey][p.titleKey] = p.rootTitle
+        first[o.attrKey] = {}
+        first[o.attrKey][o.idKey] = 0
+        first[o.attrKey][o.titleKey] = o.rootTitle
 
-        first[p.childrenKey] = tree
+        first[o.childrenKey] = tree
 
         tree = [first]
 
@@ -101,7 +108,7 @@ function tree(p) {
     } else {
         // 展开id
         tree.forEach((item) => {
-            expand.push(item[p.valueKey])
+            expand.push(item[o.valueKey])
         })
     }
 
