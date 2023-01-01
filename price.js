7f0d579c90b0bb1eaa1da19b930ba28051a56dba
junmiao <mj287@163.com>
2023-01-01 14:36:38 +0800

    price

--- price.js
+++ price.js
@@ -1,34 +1,40 @@
 const BigNumber = require('bignumber.js')
 const isNumeric = require('./isNumeric')
-const precision = require('./precision')
 const split = require('./split')
 
 /**
  * 换算金额(分转元)
  */
-function price(value, toFixed = false, defaultValue = 0, isAddComma = false, isCent = true) {
+function price(value, defaultValue = 0, isAddComma = false, isCent = true) {
 
     if (isNumeric(value)) {
 
         // 转为 BigNumber 格式
         value = new BigNumber(value)
 
-        // 如果值 === 0
-        if (value.isEqualTo(0)) {
+        // 如果值为 0
+        if (value.isZero()) {
             return 0
         }
 
         // 如果值 > 0
         if (value.isGreaterThan(0)) {
 
-            // 如果单位是分, 则除以 100
+            // 如果单位是分
             if (isCent) {
-                value = value.dividedBy(100)
-            }
-            value = value.toNumber()
+                value =
+                    // 分 除以 100
+                    value.dividedBy(100)
+                    // 转为数字
+                    .toNumber()
 
-            if (toFixed) {
-                value = precision(value, 2, toFixed)
+            // 否则是元
+            } else {
+                value =
+                    // 将元向下舍入 2 位精度(如 68.345 -> 68.34)
+                    value.decimalPlaces(2, BigNumber.ROUND_DOWN)
+                    // 转为数字
+                    .toNumber()
             }
 
             // 如果加逗号隔开
--- priceCent.js
+++ priceCent.js
@@ -10,14 +10,20 @@ function priceCent(value, defaultValue = 0) {
         // 转为 BigNumber 格式
         value = new BigNumber(value)
 
-        // 如果值 === 0
-        if (value.isEqualTo(0)) {
+        // 如果值为 0
+        if (value.isZero()) {
             return 0
         }
 
         // 如果值 > 0
         if (value.isGreaterThan(0)) {
-            return value.times(100).toNumber()
+
+            // 将元向下舍入 2 位精度(如 68.345 -> 68.34)
+            return value.decimalPlaces(2, BigNumber.ROUND_DOWN)
+                // 再乘以 100
+                .times(100)
+                // 转为数字
+                .toNumber()
         }
     }
 
