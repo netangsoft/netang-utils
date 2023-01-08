a1b938fd8b8bd9f35fb79c576164bf4e7b7d8905
junmiao <mj287@163.com>
2023-01-08 20:45:16 +0800

    filter

--- filter.js
+++ filter.js
@@ -1,12 +1,18 @@
+const _isFunction = require('lodash/isFunction')
 const _filter = require('./_filter')
 
 /**
  * 保留筛选数据
  * @param {object} data 筛选数据
- * @param {array} fields 保留筛选字段
+ * @param {array|Function} fields 保留筛选字段
  * @returns {object}
  */
 function filter(data, fields = []) {
+
+    if (_isFunction(fields)) {
+        return Array.isArray(data) ? data.filter(fields) : []
+    }
+
     return _filter(data, fields, false)
 }
 
