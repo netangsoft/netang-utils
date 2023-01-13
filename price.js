627f4686ab1a8b0f48e49f120f86c22be7a67317
junmiao <mj287@163.com>
2023-01-13 09:52:51 +0800

    price

--- price.js
+++ price.js
@@ -5,50 +5,53 @@ const split = require('./split')
 /**
  * 换算金额(分转元)
  */
-function price(value, defaultValue = 0, isAddComma = false, isCent = true) {
+function price(value, defaultValue = 0, isAddComma = false, isCent = false) {
 
     if (isNumeric(value)) {
 
         // 转为 BigNumber 格式
         value = new BigNumber(value)
 
-        // 如果值为 0
-        if (value.isZero()) {
-            return 0
-        }
+        if (value.isFinite()) {
 
-        // 如果值 > 0
-        if (value.isGreaterThan(0)) {
-
-            // 如果单位是分
-            if (isCent) {
-                value =
-                    // 分 除以 100
-                    value.dividedBy(100)
-                    // 转为数字
-                    .toNumber()
-
-            // 否则是元
-            } else {
-                value =
-                    // 将元向下舍入 2 位精度(如 68.345 -> 68.34)
-                    value.decimalPlaces(2, BigNumber.ROUND_DOWN)
-                    // 转为数字
-                    .toNumber()
+            // 如果值为 0
+            if (value.isZero()) {
+                return 0
             }
 
-            // 如果加逗号隔开
-            if (isAddComma) {
-                const arr = split(String(value), '.')
-                if (arr.length) {
-                    value = String(arr[0]).replace(/\B(?=(\d{3})+$)/g,',')
-                    if (arr.length > 1) {
-                        value += '.' + arr[1]
+            // 如果值 > 0
+            if (value.isGreaterThan(0)) {
+
+                // 如果单位是分
+                if (isCent) {
+                    value =
+                        // 分 除以 100
+                        value.dividedBy(100)
+                            // 转为数字
+                            .toNumber()
+
+                    // 否则是元
+                } else {
+                    value =
+                        // 将元向下舍入 2 位精度(如 68.345 -> 68.34)
+                        value.decimalPlaces(2, BigNumber.ROUND_DOWN)
+                            // 转为数字
+                            .toNumber()
+                }
+
+                // 如果加逗号隔开
+                if (isAddComma) {
+                    const arr = split(String(value), '.')
+                    if (arr.length) {
+                        value = String(arr[0]).replace(/\B(?=(\d{3})+$)/g,',')
+                        if (arr.length > 1) {
+                            value += '.' + arr[1]
+                        }
                     }
                 }
-            }
 
-            return value
+                return value
+            }
         }
     }
 
--- priceCent.js
+++ priceCent.js
@@ -10,20 +10,23 @@ function priceCent(value, defaultValue = 0) {
         // 转为 BigNumber 格式
         value = new BigNumber(value)
 
-        // 如果值为 0
-        if (value.isZero()) {
-            return 0
-        }
+        if (value.isFinite()) {
 
-        // 如果值 > 0
-        if (value.isGreaterThan(0)) {
+            // 如果值为 0
+            if (value.isZero()) {
+                return 0
+            }
 
-            // 将元向下舍入 2 位精度(如 68.345 -> 68.34)
-            return value.decimalPlaces(2, BigNumber.ROUND_DOWN)
-                // 再乘以 100
-                .times(100)
-                // 转为数字
-                .toNumber()
+            // 如果值 > 0
+            if (value.isGreaterThan(0)) {
+
+                // 将元向下舍入 2 位精度(如 68.345 -> 68.34)
+                return value.decimalPlaces(2, BigNumber.ROUND_DOWN)
+                    // 再乘以 100
+                    .times(100)
+                    // 转为数字
+                    .toNumber()
+            }
         }
     }
 
