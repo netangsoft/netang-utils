e3001325115397397971ec781eaf83b0ef400761
junmiao <mj287@163.com>
2023-04-27 09:38:21 +0800

    compareVersion

--- compareVersion.js
+++ compareVersion.js
@@ -1,3 +1,4 @@
+import $n_split from './split'
 import $n_trimString from './trimString'
 
 /**
@@ -9,21 +10,23 @@ import $n_trimString from './trimString'
  */
 export default function compareVersion(v1 = '0', v2 = '0') {
 
-    v1 = $n_trimString(v1)
     if (! v1) {
         v1 = '0'
     }
-    v2 = $n_trimString(v2)
     if (! v2) {
         v2 = '0'
     }
-    v1 = v1.split('.')
-    v2 = v2.split('.')
-    const minVersionLens = Math.min(v1.length, v2.length)
+    v1 = $n_split(v1, '.')
+    v2 = $n_split(v2, '.')
+
+    const count1 = v1.length
+    const count2 = v2.length
+
+    const minVersionLens = Math.min(count1, count2)
 
     for (let i = 0; i < minVersionLens; i++) {
-        const curV1 = Number(v1[i])
-        const curV2 = Number(v2[i])
+        const curV1 = v1[i]
+        const curV2 = v2[i]
 
         if (curV1 > curV2) {
             return 1
@@ -34,12 +37,23 @@ export default function compareVersion(v1 = '0', v2 = '0') {
         }
     }
 
-    if (v1.length !== v2.length) {
-        const v1BiggerThenV2 = v1.length > v2.length;
-        const maxLensVersion = v1BiggerThenV2 ? v1 : v2;
-        for (let i = minVersionLens; i < maxLensVersion.length; i++) {
-            const curVersion = Number(maxLensVersion[i])
-            if (curVersion > 0) {
+    if (count1 !== count2) {
+
+        const v1BiggerThenV2 = count1 > count2
+
+        let maxLensVersion
+        let maxLensCount
+
+        if (v1BiggerThenV2) {
+            maxLensVersion = v1
+            maxLensCount = count1
+        } else {
+            maxLensVersion = v2
+            maxLensCount = count2
+        }
+
+        for (let i = minVersionLens; i < maxLensCount; i++) {
+            if (maxLensVersion[i] > 0) {
                 return v1BiggerThenV2 ? 1 : -1
             }
         }
