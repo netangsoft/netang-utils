84283745500e13d0f9b8cdb832d3cc09af1532e7
junmiao <mj287@163.com>
2024-01-11 16:50:39 +0800

    copy

--- cjs/copy.js
+++ cjs/copy.js
@@ -1,41 +1,21 @@
 /**
  * copy
- * 复制文字/html到剪切板
+ * 复制文字到剪切板
  * @param value
- * @param isHtml 是否 html
  */
-function copy(value, isHtml = false) {
-
-    // 获取节点
-    let $dom = document.querySelector('#n-copy-content')
-    if (! $dom) {
-        $dom = document.createElement(isHtml ? 'div' : 'textarea')
-        $dom.id = 'n-copy-content'
-        $dom.contentEditable = 'true'
-        $dom.style.position = 'fixed'
-        $dom.style.top = '-999px'
-        $dom.style.left = '-999px'
-        $dom.style.width = '1px'
-        $dom.style.height = '1px'
-        document.body.appendChild($dom)
+function copy(value) {
+    let $input = document.querySelector('#n-copy-input')
+    if (! $input) {
+        $input = document.createElement('textarea')
+        $input.id = 'n-copy-input'
+        $input.contentEditable = 'true'
+        $input.style.position = 'fixed'
+        $input.style.top = '-9999px'
+        $input.style.left = '-9999px'
+        document.body.appendChild($input)
     }
-
-    if (isHtml) {
-        $dom.innerHTML = value
-        // 设置文档片段
-        const range = document.createRange()
-        range.selectNodeContents($dom)
-        // 清空选中内容
-        const selection = window.getSelection()
-        selection.removeAllRanges()
-        // 将文档片段设置为选中内容
-        selection.addRange(range)
-
-    } else {
-        $dom.value = value
-        $dom.select()
-    }
-
+    $input.value = value
+    $input.select()
     document.execCommand('copy')
 }
 
