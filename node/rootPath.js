8c86c3b1c7f2457be8868075f32cf2306be7f207
junmiao <marcus@netang.com>
2024-11-08 11:52:09 +0800

    vite

--- node/rootPath.js
+++ node/rootPath.js
@@ -1,8 +1,5 @@
 import path from 'path'
-import { fileURLToPath } from 'node:url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import { __dirname } from './fileUrlToPath.js'
 
 /**
  * 获取根路径
